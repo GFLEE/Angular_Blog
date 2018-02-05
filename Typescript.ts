569e9423f03463146f4cd5dcdad9a65c4eee70af
gflee <leegradylll@gmail.com>
2018-02-05 21:23:17 +0800

    =>

--- Typescript.ts
+++ Typescript.ts
@@ -51,7 +51,36 @@
 // tab.run();
 
 //==============================
-var data:string[]=['one','two','three'];
-data.forEach(function(datas){
-    console.log(datas);
-})
+// var data:string[]=['one','two','three'];
+// data.forEach(function(datas){
+//     console.log(datas);
+// })
+
+// data.forEach((m)=>console.log(m));    //快速书写函数,一个参数时，m的括号可省略
+
+// var numbers:number[]=[1,2,3,4,5];
+// numbers.map((m)=>{
+//     m=m+1;console.log(m)
+// }); //Map方法，返回经过操作后的数组
+//而且=>语法和外部代码拥有同样的this；
+
+
+var man={
+    name:"lee",
+    age:20,
+    like:'guitar',
+    guitars:['taylor','martin'],
+
+     get:function(guitars){
+          this.guitars.forEach(function(g){
+              console.log(g)
+          })
+     }
+
+}
+
+
+
+
+//箭头函数是处理内联函数的好办法。这也让我们在JavaScript中更容易使用高阶函数。
+
