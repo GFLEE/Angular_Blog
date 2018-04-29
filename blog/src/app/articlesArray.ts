0ed339885bfd49c360c8d84ab1d2f650c26a54cb
Lee <leegradylll@gmail.com>
2018-04-29 14:17:26 +0800

    detal_component

--- blog/src/app/articlesArray.ts
+++ blog/src/app/articlesArray.ts
@@ -2,32 +2,32 @@ import { Article } from './article';
 
 export const articles: Article[] = [
         // tslint:disable-next-line:max-line-length
-        { _id: '1', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['React'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+        { _id: '1', title: 'Header1', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['React'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
         // tslint:disable-next-line:max-line-length
-        { _id: '2', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Javascript'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+        { _id: '2', title: 'Header2', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Javascript'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
         // tslint:disable-next-line:max-line-length
-        { _id: '3', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Vue'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+        { _id: '3', title: 'Header3', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Vue'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
         // tslint:disable-next-line:max-line-length
-        { _id: '4', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['angular'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+        { _id: '4', title: 'Header4', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['angular'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
 
  // tslint:disable-next-line:max-line-length
- { _id: '1', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['React'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+ { _id: '5', title: 'Header6', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['React'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
  // tslint:disable-next-line:max-line-length
- { _id: '2', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Javascript'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+ { _id: '6', title: 'Header7', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Javascript'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
  // tslint:disable-next-line:max-line-length
- { _id: '3', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Vue'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+ { _id: '7', title: 'Test8', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Vue'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
  // tslint:disable-next-line:max-line-length
- { _id: '4', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['angular'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+ { _id: '8', title: 'Test9', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['angular'], summary: 'summary.....', html: 'htmlcontent', date: '6 / 13 / 2017' },
 
 
   // tslint:disable-next-line:max-line-length
-  { _id: '1', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['React'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+  { _id: '9', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['React'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
   // tslint:disable-next-line:max-line-length
-  { _id: '2', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Javascript'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+  { _id: '10', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Javascript'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
   // tslint:disable-next-line:max-line-length
-  { _id: '3', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Vue'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+  { _id: '11', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['Vue'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
   // tslint:disable-next-line:max-line-length
-  { _id: '4', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['angular'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
+  { _id: '12', title: 'Test', word: 421, view: 32, comment: 32, comments: ['wqw', 'wqw'], labels: ['angular'], summary: 'summary', html: 'htmlcontent', date: '6 / 13 / 2017' },
 
 
 
--- blog/src/app/detail/detail.component.ts
+++ blog/src/app/detail/detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
+import { ActivatedRoute, Params } from '@angular/router';
+
 
 
 @Component({
@@ -10,19 +12,33 @@ import { AppRoutingModule } from '../app-routing/app-routing.module';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-  @Input() article: Article;
+  public id: string;
+  article: Article;
+  articles: Article[];
   // article: Article = new Article();
   constructor(
     private articleService: ArticleService,
     // private routing: AppRoutingModule
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+        console.log(this.id);
+      }
+    );
+    this.articles = this.articleService.getArticles();
     this.getArticle();
   }
 
   getArticle(): void {
 
-       
+    // tslint:disable-next-line:radix
+    const _id = parseInt(this.id);
+    console.log(_id);
+
+    this.article = this.articles[_id - 1];
   }
 }
